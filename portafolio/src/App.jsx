c9d985857a096c25de8012a88bc5f37cfec7f4d8
MaicolZzz <buitragomaicol23@gmail.com>
2025-12-02 19:11:48 -0500

    Error corregido en netlify

--- portafolio/src/App.jsx
+++ portafolio/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import React from 'react'
 import Sidebar from './components/barra';
 import MainContent from './components/contenedorPrincipal';
 import './styles/App.css';
